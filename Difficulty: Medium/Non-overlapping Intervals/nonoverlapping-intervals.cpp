956551d779da182734de2a056f0e5de0120ef5e0
SELVANAGARAJAN M <144997943+Selvarajan-M@users.noreply.github.com>
2025-06-13 12:26:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Non-overlapping Intervals/nonoverlapping-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    static bool comp(const vector<int>& val1, const vector<int>& val2){
+        return val1[1] < val2[1];
+    }
+    int minRemoval(vector<vector<int>> &intervals) {
+        // code here
+        sort(intervals.begin(),intervals.end(),comp);
+        int cnt=1,lastEndTime=intervals[0][1],n=intervals.size();
+        for(int i=1;i<n;i++){
+            if(intervals[i][0] >= lastEndTime){
+                cnt=cnt+1;
+                lastEndTime=intervals[i][1];
+            }
+        }
+        return n-cnt;
+    }
+};
