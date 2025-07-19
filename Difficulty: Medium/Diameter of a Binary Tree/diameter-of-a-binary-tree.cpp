903d280f11a9f9a5e9d5941bfb22d50695fc6302
SELVANAGARAJAN M <144997943+Selvarajan-M@users.noreply.github.com>
2025-07-19 11:51:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.cpp
@@ -0,0 +1,38 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* newNode(int val) {
+    return new Node(val);
+}
+*/
+
+class Solution {
+  public:
+    int diameter(Node* root) {
+        // Your code here
+        int diameter=0;
+        height(root, diameter);
+        return diameter;
+    }
+  private:
+    int height(Node* root, int& diameter){
+        if(root == NULL)
+        return 0;
+        int lh=height(root->left, diameter);
+        int rh=height(root->right, diameter);
+        diameter=max(diameter, lh+rh);
+        return 1+max(lh,rh); 
+    }
+
+};
